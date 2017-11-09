0b4f7f5c461d4c2111798f7e5e5ecca83a3d2328
audrynyonata <audrynyonata@gmail.com>
2017-11-09 07:28:46 +0700

    chatwindow div

---
+++ index.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+  <title>
+    NewsUp by NewsFinders
+  </title>
+  <link href="css/master.css" rel="stylesheet">
+  
+</head>
+<body>
+  <div class="chatwindow"></div>
+</body>
+</html>
